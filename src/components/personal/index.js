9397e9c3d175af20c1f3ecc26ee9089da436ab2e
Soy Sin <sins.morakot@gmail.com>
2023-02-19 15:48:35 +0700

    add alert message!

--- src/components/personal/index.js
+++ src/components/personal/index.js
@@ -4,6 +4,12 @@ import "./style.css";
 function Personal() {
   return (
     <div className="container-personal">
+      <div className="noted-card">
+        <p className="color-yellow">
+          Thank you for visit my website. Now I am not available unit 12 July
+          2023!!
+        </p>
+      </div>
       <h1 className="color-yellow">Mobile Developer</h1>
       <p>
         I am working at Morakot Technology on position Mobile Developer. I have
